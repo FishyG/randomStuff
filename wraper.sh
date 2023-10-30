c4fcfb8e659131134c774d76b72ebca242a30652
Jessy <triogaming122@gmail.com>
2023-10-29 23:32:52 -0400

    Wraper done :3

--- wraper.sh
+++ wraper.sh
@@ -1,34 +1,92 @@
 #!/bin/bash
 
+#Colors (use ${COLOR_NAME})
+CLEAR='\033[0m'
+RED='\033[1;31m'
+PURPLE='\033[1;35m'
+
 DUCK_RENDER=~/.ducky/duck_render
-if [ -f "$DUCK_RENDER" ]; then
-		echo "$DUCK_RENDER exists."
+if [ -f "$DUCK_RENDER" ]
+then
+	echo "$DUCK_RENDER exists."
+else
+	echo -e "${PURPLE}Cweating \"~/.ducky\" diwectowy >.< ...${CLEAR}"
+	sleep 3
+	mkdir -p ~/.ducky
+
+	# Getting the ascii art from github
+	echo -e "${PURPLE}Getting the x3 ascii awt OwO...${CLEAR}"
+	sleep 3
+	if command -v wget &> /dev/null
+	then
+		wget -P ~/.ducky/ https://raw.githubusercontent.com/FishyG/randomStuff/main/duck.ascii	
 	else
-		echo "Cweating \"~/.ducky\" diwectowy >.< ..."
-		mkdir -p ~/.ducky
 
-		echo "Getting the x3 ascii awt OwO..."
-		if command -v <wget> &> /dev/null
+		echo -e "${RED}Oh nyo, w-w-wget (・\`ω´・) nyot found *confusion*${CLEAR}"
+		sleep 3
+		echo -e "${PURPLE}Imma twy with curl ^w^...${CLEAR}"
+		sleep 3
+		if command -v curl &> /dev/null
 		then
-			wget
+			curl -so ~/.ducky/duck.ascii https://raw.githubusercontent.com/FishyG/randomStuff/main/duck.ascii
 		else
+			echo -e "${RED}Nyot even OwO curl ?!! ${CLEAR}"
+			sleep 3
 
-			echo "Oh nyo, w-w-wget (・\`ω´・) nyot found *confusion*"
-			if command -v <curl> &> /dev/null
-			then
-				curl
-			else
-				echo "Nyot even OwO curl ?!!"
-				# Install wget if not present
-				echo "Don't wowwy, imma instawl wget fow you >w<"
-				sudo apt-get install -y wget
-				wget 
-			fi
+			# Install wget if not present
+			echo -e "${PURPLE}Don't wowwy, imma instawl i-it fow you >w<${CLEAR}"
+			sleep 3
+			sudo apt-get install -y curl
+			curl -so ~/.ducky/duck.ascii https://raw.githubusercontent.com/FishyG/randomStuff/main/duck.ascii
 		fi
+	fi
 
+	# Getting the rendering script from github 
+	echo -e "${PURPLE}Getting the x3 wendewing scwipt...${CLEAR}"
+	sleep 3
+	if command -v wget &> /dev/null
+	then
+		wget -P ~/.ducky/ https://raw.githubusercontent.com/FishyG/randomStuff/main/renderer.sh
+	else
+		echo -e "${RED}Oh nyo, w-w-wget (・\`ω´・) nyot found *confusion*${CLEAR}"
+		sleep 3
+		echo -e "${PURPLE}Imma twy with curl ^w^...${CLEAR}"
+		sleep 3
+		curl -so ~/.ducky/renderer.sh https://raw.githubusercontent.com/FishyG/randomStuff/main/renderer.sh
+	fi
+	chmod +x ~/.ducky/renderer.sh
 
-
-
+	echo -e "${PURPLE}Stawting ( ੭•͈ω•͈)੭ the x3 wendewing of the ducky...${CLEAR}"
+	sleep 3
+	if ! command -v lolcat &> /dev/null
+	then
+		echo -e "${RED}You fowgot t-to instaww \"lolcat\" you siwwy *looks at you* goose :3${CLEAR}"
+		sleep 3
+		echo -e "${PURPLE}Don't wowwy, imma instawl i-it fow you ( ˶ˆ꒳ˆ˵ )${CLEAR}"
+		sudo apt install -y lolcat
+	fi
+	cat ~/.ducky/duck.ascii | ~/.ducky/renderer.sh $HOSTNAME > $DUCK_RENDER
 fi
 
+clear
+echo -e "${PURPLE}DUCK MAN YAAAAAAAAAAAAAAAAAAAAOOOOOHHHHH${CLEAR}"
+sleep 1
+echo ""
+echo "press Q to exit :3"
+sleep 3
+clear
+tput cup 0 0
+tput invis
+while true
+do  
+	awk '1;/\[[0-9]+A/{system("sleep .05")}' $DUCK_RENDER
+	read -t 0.05 -N 1 input; 
+	if [[ $input = "q" ]] || [[ $input = "Q" ]]
+	then
+		clear
+		echo -e "${PURPLE}Bye bye UwU${CLEAR}"
+		break
+	fi
+tput cup 0 0
+done
 
